fc701925f59a22778f2fc9280946b099345c7425
RickM2000 <riccardomacor12@gmail.com>
2023-03-07 20:33:53 +0100

    Win script and finish project

--- Fine modulo/Assets/Scripts/Finish.cs
+++ Fine modulo/Assets/Scripts/Finish.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Finish : MonoBehaviour
 {
+    public GameObject youWinText;
+    public float delay;
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        youWinText.SetActive(false);
+    }
+   public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            youWinText.SetActive(true);
+            StartCoroutine(Countdown());
+        }
+    }
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds (delay);
+        SceneManager.LoadScene(0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+    
 }
